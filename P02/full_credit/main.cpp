41f50d72c3a499db6870fcc0abb3b78cb63981f8
zaky qalawi <zaky.qalawi@mavs.uta.edu>
2020-09-08 01:43:13 -0500

    final source code

--- P02/full_credit/main.cpp
+++ P02/full_credit/main.cpp
@@ -4,7 +4,7 @@
 
 int main(){
 srand(time(NULL));
-int num_die, num_face, num_rolls,i,sum=0,j;
+int num_die, num_face, num_rolls,i,sum,j;
 
 
 std::cout<< "How many dice will be rolled? "<<std::endl;
@@ -21,6 +21,7 @@ int num[num_die];
 
 
 for(i=0; i<num_rolls; i++){
+sum=0;
 
 std::cout<<"\nRoll "<<i<<": ";
 
@@ -28,9 +29,9 @@ for(j=0; j<num_die; j++){
 
 num[j]=(rand()%6+1);
 std::cout<<num[j]<<" ";
-
+sum = sum+num[j];
 }
-
+std::cout<<"Sum: "<<sum;
 std::cout<<"\n";
 
 }
