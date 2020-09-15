ba9b88dd1d57c5268ad2fe4c71747769f821f7f4
zaky qalawi <zaky.qalawi@mavs.uta.edu>
2020-09-15 06:28:16 -0500

    added all varibles and methods to header file

--- P03/full_credit/tile.h
+++ P03/full_credit/tile.h
@@ -1,18 +1,20 @@
 #ifndef TILE_H
 #define TILE_H
+
 #include<string>
 #include<iostream>
-#include<string>
 
-
-
-class tile{
+class Tile{
 
 public:// holds methods, constructers, and static methods
-    tile();
-    ~tile();
+    Tile(std::string word);
+    ~Tile();
     int width(); // returns width
     std::string word();
+    void set_matched();
+    bool match(Tile &tile);
+    bool matched();
+    std::string to_string();
 
 
 private:// holds variables and static 
@@ -20,7 +22,7 @@ private:// holds variables and static
 std::string _word;
 bool _matched;
  static int _width;
-static std::string blank;
+static std::string _blank;
 
 };
 
