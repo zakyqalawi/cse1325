84e8f9dd19e8a80164217f40ff9b9426834b81bc
zaky qalawi <zaky.qalawi@mavs.uta.edu>
2020-09-25 17:58:08 -0500

    added constructor and connect method

--- P05/full_credit/Gate.cpp
+++ P05/full_credit/Gate.cpp
@@ -2,4 +2,8 @@
 
 Gate::Gate (Signal input1, Signal input2):_input1{input1}, _input2{input2}{}
 
+void Gate::connect(Gate& gate, Pin input_pin){
+	_to_gate = &gate;
+	_to_pin=input_pin;
+}
 
--- P05/full_credit/main.cpp
+++ P05/full_credit/main.cpp
@@ -1,6 +1,8 @@
 #include"Gate.h" 
 
 int main(){
+
+
 return 0;
 }
 
