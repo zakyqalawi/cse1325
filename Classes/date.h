de4f9d42f25a38cef303d9aa3339517f3bd2e65a
zaky qalawi <zaky.qalawi@mavs.uta.edu>
2020-10-19 23:17:47 -0500

    Added CML interface for P06

---
+++ Classes/date.h
@@ -0,0 +1,10 @@
+#include<iostream>
+
+class Date{
+ int month, day, year;
+ public:
+        Date(int _month, int _day, int _year);
+        void print_();
+       
+        
+};
---
+++ Classes/main.cpp
@@ -0,0 +1,9 @@
+ #include"date.h"
+
+int main(){
+
+Date birthday{2,4,2000};
+
+birthday.print_();
+
+}
